ce3d971826667dc725336265250471bdf90d5695
Allan Nielsen <alnig441@gmail.com>
2019-02-03 15:20:44 -0500

    view and query improvements

--- routes/api_v1.js
+++ routes/api_v1.js
@@ -11,18 +11,13 @@ photoSchema.plugin(paginate);
 const Photo = mongoose.model('Photo', photoSchema);
 const User = mongoose.model('User', userSchema);
 
-var test_photo = {
-    image: {
-        fileName: 'test.jpg'
-    }
-};
-
 router.get('/SearchById/:_id?/Photos', (req, res, next) => {
 
-    let query = buildQuery(req.params);
+    let query = { _id: req.params };
 
     Photo.paginate(query)
         .then((image) => {
+            console.log(image);
             res.render('results', {docs: image.docs, endpoint: 'photos'})
         })
         .catch((error) => {
@@ -33,12 +28,11 @@ router.get('/SearchById/:_id?/Photos', (req, res, next) => {
 
 router.get('/Search/Photos?', userAuth, (req, res, next) => {
 
-    let query = buildQuery(req.query);
-
     let options = { page: parseInt(req.query.page) , limit: parseInt(process.env.LIMIT) }
 
-    Photo.paginate(query , options)
+    Photo.paginate({} , options)
         .then((result) => {
+            console.log(result.docs[0]);
             res.render('results', {docs: result, endpoint: 'photos'})
         })
         .catch((error) => {
@@ -51,13 +45,15 @@ router.post('/UpdateById/:_id?/Photos',(req, res, next) => {
 
     /* FIX QUERY BUILDER TO CONFORM TO {'field':'value'} OR {'object.field': 'value'} SYNTAX */
 
-    // let query = buildQuery(req.body);
+    let incoming = req.body ? req.body : req.query;
 
+    let query = parseQuery(incoming);
+    console.log(query, incoming);
 
     Photo.findOneAndUpdate({_id: req.params._id}, query)
         .then((photo) => {
             console.log(photo);
-            res.render('results', { docs: JSON.stringify(photo)});
+            res.render('results', { docs: photo, endpoint: 'photos'});
         })
         .catch((error) => {
             res.render('error', { message: error });
@@ -67,12 +63,14 @@ router.post('/UpdateById/:_id?/Photos',(req, res, next) => {
 
 router.post('/Load/Photos', (req, res, next) => {
 
-    let photo = new Photo(test_photo);
+    let request = buildDocument(req.body);
+
+    let photo = new Photo(request);
     photo.set({ created: new Date });
 
     photo.save()
         .then((result) => {
-            res.render('results', { docs: JSON.stringify(result) })
+            res.render('results', { docs: result, endpoint: 'photos' })
         })
         .catch((error) => {
             res.render('error', { message: error })
@@ -96,9 +94,44 @@ router.delete('/Remove/:_id?/Photos', (req, res, next) => {
 });
 
 
+function parseQuery(request) {
+    let query;
 
+    Object.keys(request).forEach((key) => {
+        if (key != 'page') {
+            switch (key) {
+                case 'city':
+                    return  query = { "location.city" : request[key] };
+                case 'state':
+                    return  query = { "location.state" : request[key] };
+                case 'country':
+                    return  query = { "location.country" : request[key] };
+                case 'day':
+                    return  query = { "date.day" : parseInt(request[key]) };
+                case 'month':
+                    return  query = { "date.month" : parseInt(request[key])};
+                case 'year':
+                    return  query = { "date.year" : parseInt(request[key]) };
+                case 'names':
+                    return  query = { "meta.names" : request[key] };
+                case 'venue':
+                    return  query = { "meta.venue" : request[key] };
+                case 'occasion':
+                    return  query = { "meta.occasion" : request[key] };
+                case 'fileName':
+                    return  query = { "image.fileName" : request[key] };
+                case 'thumbnail':
+                    return  query = { "image.thumbnail" : request[key] };
+                default:
+                    return null;
+            }
+        }
+    })
 
-function buildQuery(req) {
+    return query;
+}
+
+function buildDocument(req) {
 
     let query = {};
 
