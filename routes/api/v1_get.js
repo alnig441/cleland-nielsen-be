e44ccac7728030184bf54739f124fa292e6020d8
Allan Nielsen <alnig441@gmail.com>
2019-03-01 17:12:08 -0500

    clean-up

--- routes/api/v1_get.js
+++ routes/api/v1_get.js
@@ -36,8 +36,6 @@ router.get('/SearchById/:_id?/Photos', (req, res, next) => {
 router.get('/Search/Photos?', (req, res, next) => {
 
     let options = { page: parseInt(req.query.page) || undefined , limit: parseInt(process.env.LIMIT) };
-    // let doAnd = req.query.doAnd ? true: false ;
-
     let query = parser.parseSearchQuery(req.query, req.query.doAnd);
 
     Photo.paginate( query , options )
