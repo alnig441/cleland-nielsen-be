f5e6e7b680785d9ddb395199be17962e0dac1a9c
Allan Nielsen <alnig441@gmail.com>
2019-04-22 16:14:22 -0400

    post route refactored to use ModelParser module

--- routes/api/v1_post.js
+++ routes/api/v1_post.js
@@ -3,12 +3,14 @@ const router = express.Router();
 const mongoose = require('mongoose');
 const parser = require('../../app_modules/parser');
 const ObjectId = require('mongodb').ObjectId;
-
+const RequestParser = require('../../app_modules/model-parser');
 const userSchema = require('../../schemas/schemas').userSchema;
 const photoSchema = require('../../schemas/schemas').photoSchema;
 
 const Photo = mongoose.model('Photo', photoSchema);
 const User = mongoose.model('User', userSchema);
+
+const PhotoRequest = new RequestParser(Object.keys(photoSchema.paths));
 
 router.post('/UpdateById/:_id?/Photos',(req, res, next) => {
 
@@ -16,13 +18,14 @@ router.post('/UpdateById/:_id?/Photos',(req, res, next) => {
 
     query = parser.parse(incoming);
 
-    console.log('query single: ', query)
+    let test = PhotoRequest.parse(incoming);
 
-    Photo.updateOne({ _id: req.params._id }, query)
+    Photo.updateOne({ _id: new ObjectId(req.params._id) }, query)
       .then(result => {
         if (req.accepts().includes('*/*') || req.accepts().includes('text/html')) {
           res.render('results', { docs: result, endpoint: 'photos' })
         } else {
+          console.log('result: ', result)
           res.format({
             json: () => {
               res.send(result);
@@ -33,51 +36,6 @@ router.post('/UpdateById/:_id?/Photos',(req, res, next) => {
       .catch(error => {
         res.render('error: ', { message: error })
       })
-
-    // let goTo = stepThrough(incoming);
-    //
-    // parseIncoming();
-    //
-    // function parseIncoming() {
-    //     let result = goTo.next();
-    //     updatePhoto(result);
-    // }
-    //
-    // function updatePhoto ( field ) {
-    //     Photo.findOneAndUpdate({_id: req.params._id}, field.value)
-    //         .then(( photo ) => {
-    //             if ( !field.done ) {
-    //                 parseIncoming();
-    //             } else {
-    //               if ( req.accepts().includes('*/*') || req.accepts().includes('text/html') ) {
-    //                 res.render('results', {docs: photo, endpoint: 'photos'})
-    //               } else {
-    //                 res.format({
-    //                   json: () => {
-    //                     res.send(photo);
-    //                   }
-    //                 })
-    //               }
-    //             }
-    //         })
-    //         .catch(( error ) => {
-    //             res.render('error', { message: error });
-    //         })
-    // }
-    //
-    // function* stepThrough ( object_in ) {
-    //     let i = 0;
-    //     let keys = Object.keys( object_in );
-    //
-    //     while(i <= keys.length -1) {
-    //         i++;
-    //         let object_out = {};
-    //         object_out[keys[i - 1]] = object_in[keys[i - 1]];
-    //         yield parser.parseUpdateQuery(object_out);
-    //     }
-    //
-    //     return;
-    // }
 })
 
 router.post('/Update/Photos?', (req, res, next) => {
@@ -89,7 +47,9 @@ router.post('/Update/Photos?', (req, res, next) => {
     _ids.push({_id: new ObjectId(id)});
   })
 
-  let query = parser.parse(incoming['form']);
+  // let query = parser.parse(incoming['form']);
+
+  let query = PhotoRequest.parse(incoming['form']);
 
   Photo.updateMany({ $or: _ids }, query)
     .then(result => {
